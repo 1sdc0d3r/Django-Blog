a676c4f9d9c4d731af139458faed3c762e72ba44
Braden <fish0859@gmail.com>
2020-05-14 14:08:04 -0600

    added a base blog template w/ bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Braden',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'May 14, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'May 15, 2020'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
