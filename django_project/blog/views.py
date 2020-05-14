92b258aade040c700591768879707cac466bf164
Braden <fish0859@gmail.com>
2020-05-14 16:10:08 -0600

    added some entries using queries in python bash. Changed posts from mock data to query from database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Braden',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'May 14, 2020'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'May 15, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
