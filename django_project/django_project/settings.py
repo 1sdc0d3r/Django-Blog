7dedab61082e98f7525aafa3bbbc9e3effe767a1
Braden <fish0859@gmail.com>
2020-05-21 12:15:34 -0600

    updated Procfile, settings and requirements.txt, Hosted on Heroku

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,13 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm1slv66!-226@e6hr3qg3yeq&rb565&nrci_gf-k@)rsso4*(@'
+# SECRET_KEY = 'm1slv66!-226@e6hr3qg3yeq&rb565&nrci_gf-k@)rsso4*(@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-blog-123.herokuapp.com']
 
 
 # Application definition
@@ -148,3 +151,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())  # auto set settings
